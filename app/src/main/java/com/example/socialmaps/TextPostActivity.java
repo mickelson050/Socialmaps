c95f9c833fe50ea6a130080120d12a6f3ca31b55
GreatGandalf <levi.staal15@gmail.com>
2019-06-24 23:05:07 +0200

    Fixed http libs

--- app/src/main/java/com/example/socialmaps/TextPostActivity.java
+++ app/src/main/java/com/example/socialmaps/TextPostActivity.java
@@ -22,6 +22,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class TextPostActivity extends AppCompatActivity {
 
     private static final String TAG = "TextPostActivity";
