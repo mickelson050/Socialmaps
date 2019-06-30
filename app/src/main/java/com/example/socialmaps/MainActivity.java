df4c06706500160db404c24cbd548882b2758ae7
GreatGandalf <levi.staal15@gmail.com>
2019-06-30 15:39:33 +0200

    Added MD5 hash to login

--- app/src/main/java/com/example/socialmaps/MainActivity.java
+++ app/src/main/java/com/example/socialmaps/MainActivity.java
@@ -36,6 +36,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 import static com.example.socialmaps.util.Constants.ERROR_DIALOG_REQUEST;
@@ -115,11 +118,42 @@ public class MainActivity extends AppCompatActivity {
                 minDistance, mLocationListener);
     }
 
+    private static String getmd5(String password){
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(password.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void login() {
+
+        String hashedPass = getmd5(loginPassword.getText().toString());
+        Log.v(TAG, hashedPass);
+
         HashMap<String, String> loginMap = new HashMap<String, String>();
 
         loginMap.put("email", loginEmail.getText().toString());
-        loginMap.put("password", loginPassword.getText().toString());
+        loginMap.put("password", hashedPass);
 
         t = new TestSender();
         t.doThePost("http://socialmaps.openode.io/api/login",loginMap);
